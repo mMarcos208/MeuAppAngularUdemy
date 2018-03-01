a5ce1a33fc4bb6bfe16dff3570768f6d16fea43f
marconi <marconimbarroso@gmail.com>
2018-03-01 16:47:41 -0300

    Adionando animação no Angular

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -1,17 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import {
+  trigger,
+  state,
+  transition,
+  animate,
+  style
+} from "@angular/animations";
 @Component({
-  selector: 'mt-snackbar',
-  templateUrl: './snackbar.component.html',
-  styleUrls: ['./snackbar.component.css']
+  selector: "mt-snackbar",
+  templateUrl: "./snackbar.component.html",
+  styleUrls: ["./snackbar.component.css"],
+  animations: [
+    trigger("snack-visible", [
+      state(
+        "hidden",
+        style({
+          opacity: 0,
+          bottom: 0
+        })
+      ),
+      state(
+        "visible",
+        style({
+          opacity: 1,
+          bottom: "30px"
+        })
+      ),
+      transition("hidden => visible", animate("500 ms 0s ease-in")),
+      transition("visible => hidden", animate("500 ms 0s ease-out"))
+    ])
+  ]
 })
 export class SnackbarComponent implements OnInit {
+  mesage: string = "Hello SnackBar";
+  snackVisible: string = "hidden";
+  constructor() {}
 
-  mesage: string = 'Hello SnackBar';
-  
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
